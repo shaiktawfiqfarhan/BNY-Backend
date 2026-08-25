c3689629ee20649d58e1a5ce66442e93d903828d
Shaik TawfiqFarhan <shaik.tawfiqfarhan@mphasis.com>
2026-08-25 11:05:17 +0530

    Update

--- src/main/java/com/Backend/controller/TrainingController.java
+++ src/main/java/com/Backend/controller/TrainingController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,6 @@ import com.Backend.dto.ApiResponse;
 import com.Backend.dto.TrainingRequest;
 import com.Backend.dto.TrainingResponse;
 import com.Backend.service.TrainingService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api")
